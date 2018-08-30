570027becdb24422fd3bc93e252f550488d6c122
Kartheek Raj <36413247+kartheekraj@users.noreply.github.com>
2018-08-30 15:27:17 +0530

    First push

---
+++ NLP01.py
@@ -0,0 +1,11 @@
+import nltk
+
+
+#To download all the nltk tool kit execute the following code chunk
+nltk.download()
+
+
+
+#Tokenizing(Grouping things)- word tokenizers.., sentence tokenizers
+#corporas - body of text(Medical journals, presidential speeches etc or anything in english language).
+#lexicon - words and their meanings
---
+++ NLP03(Stop words).py
@@ -0,0 +1,27 @@
+#stop words
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+example = "This is an example showing off stop word filteration."
+stop_words = set(stopwords.words("english"))
+
+#print(stop_words) #prints all the stopwords
+
+words = word_tokenize(example)
+
+'''
+filtered_sentence = []
+
+for w in words:
+    if w not in  stop_words:
+        filtered_sentence.append(w)
+
+print(filtered_sentence)
+'''
+
+#Alternative
+
+filtered_sentence = [w for w in words if not w in stop_words]
+
+print(filtered_sentence)
